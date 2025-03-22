96d2d64c5b1278c850e66f12a2d3b5188365b8f7
Flávio Félix <fau_felix30@hotmail.com>
2025-03-22 19:31:44 -0300

    feat(frontend): Feito o readme do github

--- frontend/src/app/(paginas)/projeto/[id]/page.tsx
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -1,14 +1,29 @@
+import Readme from "@/components/projetos/Readme";
 import Cabecalho from "@/components/shared/Cabecalho";
+import CarrosselImagens from "@/components/shared/CarroselImagens";
+import Container from "@/components/shared/Container";
+import ConteudoMD from "@/components/shared/conteudoMD";
+import Tecnologias from "@/components/tecnologias/Tecnologias";
+import { obterReadme } from "@/functions/github";
+import { obterProjeto } from "@/functions/projetos";
 
 export default async function PaginaProjeto(props: { params: Promise<{ id: string }> }) {
 	const { id } = await props.params;
+	const projeto = await obterProjeto(id);
+
+	if (!projeto) return null;
+
+	const readme = await obterReadme(projeto.repositorio);
 
 	return (
 		<div className="bg-500">
 			<Cabecalho />
-			<div>
-				<h1>Projeto {id}</h1>
-			</div>
+			<Container className="flex flex-col gap-10 py-7">
+				<h1 className="text-3xl font-bold self-start">{projeto.nome}</h1>
+				<CarrosselImagens imagens={projeto.imagens.slice(1)} />
+				<Tecnologias lista={projeto.tecnologias} tamanhoMenor />
+				<Readme markdown={readme} />
+			</Container>
 		</div>
 	);
 }
